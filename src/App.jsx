4c23c2e645774d276ef90b47b0da61f4643e3dfd
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2023-02-26 23:37:03 +0300

    Add particles background, improve styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
+import React, { useCallback } from 'react';
+
+import Particles from 'react-particles';
+import { loadFull } from 'tsparticles';
+import tsParticlesOptions from './assets/particles.json';
+
 import Navigation from './components/Navigation/Navigation';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+
 import './App.css';
 
+
 function App() {
+    const particlesInit = useCallback(async engine => {
+        await loadFull(engine);
+    }, []);
+
     return (
         <div className="App">
             <Navigation />
             <Rank />
             <ImageLinkForm />
+            <Particles
+                id="tsparticles"
+                options={tsParticlesOptions}
+                init={particlesInit}
+            />
         </div>
     );
 }
