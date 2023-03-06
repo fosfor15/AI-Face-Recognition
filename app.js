1c7bdb71dda9b207dfb435a84309b16e80529805
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2023-03-07 00:30:42 +0300

    Create /signin and /register endpoints

--- app.js
+++ app.js
@@ -3,8 +3,22 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('The app is working');
+    res.send('We get your GET request');
+});
+
+app.post('/signin', (req, res) => {
+    res.send('We get your POST request');
+    console.log(req.body);
+});
+
+app.post('/register', (req, res) => {
+    res.send('We get your POST request');
+    console.log(req.body);
+    res.send('Registration is done');
 });
 
 app.listen(port, () => {
