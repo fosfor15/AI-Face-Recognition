8793d2bcac6d21782cca9ab85c229884ccf5c94c
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2023-03-25 22:40:16 +0300

    Update axiosInstance to new basURL

--- frontend/src/network/axios-instance.js
+++ frontend/src/network/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://ai-face-recognition-backend.onrender.com',
     headers: { 'Content-Type': 'application/json; charset=utf-8' }
 });
 
