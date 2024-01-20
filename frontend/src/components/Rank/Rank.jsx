3098b2f0cb5b9595cc0b11f25b54de10806d819d
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2024-01-20 16:47:30 +0300

    Add badge system to rank entries
    Signed-off-by: Dmitry Kostyuk <dima.kostyuk1508@gmail.com>

--- frontend/src/components/Rank/Rank.jsx
+++ frontend/src/components/Rank/Rank.jsx
@@ -3,13 +3,20 @@ import AuthContext from '../../context/AuthContext';
 
 import './Rank.css';
 
+const entriesBadges = [
+    '🙂', '😉', '🫡', '🙃', '😀', '😄', '😁', '😎', '😺', '👻', '👾', '👽', '🚀', '💫', '✨'
+];
 
 const Rank = () => {
     const { user: { name, entries } } = useContext(AuthContext);
+    const badge = entries < entriesBadges.length
+        ? entriesBadges[entries]
+        : entriesBadges.at(-1);
 
     return (
         <div className='rank'>
             <p>Dear { name }, your rank is #{ entries }</p>
+            <p>Your badge is { badge }</p>
         </div>
     );
 }
